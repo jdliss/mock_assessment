a20a6a584e99d8ceb53d74b5ddc79a5dc196ba77
Jonathan <jonacom@lissismore.com>
2016-07-25 10:53:16 -0600

    implementing links

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new", as: :new_user
   post "/users", to: "users#create"
+
   get "/login", to: "sessions#new", as: :login
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy", as: :logout
+
+  post "/links", to: "links#create"
 end
 
