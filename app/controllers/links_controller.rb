ac792a96e4f3d83bf6b7b1baf07423dbb3762648
Jonathan <jonacom@lissismore.com>
2016-07-25 11:10:33 -0600

    can add links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -6,4 +6,22 @@ class LinksController < ApplicationController
       redirect_to login_path
     end
   end
+
+  def create
+    link = Link.new(link_params)
+    link.user = current_user
+    if link.save
+      flash[:notice] = "link added!"
+      redirect_to root_path
+    else
+      flash[:error] = "invalid link"
+      redirect_to root_path
+    end
+  end
+
+private
+
+  def link_params
+    params.permit(:title, :url)
+  end
 end
